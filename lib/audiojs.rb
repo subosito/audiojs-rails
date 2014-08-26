95391494c672a6383502420755cd62c10bd1e314
Alif Rachmawadi <subosito@gmail.com>
2014-08-26 17:35:01 +0700

    registering audiojs assets to assets precompilation

--- lib/audiojs.rb
+++ lib/audiojs.rb
@@ -3,6 +3,9 @@ require "audiojs/version"
 module Audiojs
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'audiojs.assets.precompile' do |app|
+        app.config.assets.precompile += %w( audiojs-player-graphics.gif audiojs.swf )
+      end
     end
   end
 end
